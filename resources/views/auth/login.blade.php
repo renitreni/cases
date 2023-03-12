97fcc4f6ec0e2bf819cdf7bd422353f47be0f9a7
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-03-12 15:11:27 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,7 +35,7 @@
                                         <div class="edit-profile__body">
                                             <div class="form-group mb-20">
                                                 <label for="email">Username Or Email Address</label>
-                                                <input type="text" class="form-control" id="email" name="email" value="admin@gmail.com" placeholder="Email address">
+                                                <input type="text" class="form-control" id="email" name="email" placeholder="Email address">
                                                 @if($errors->has('email'))
                                                     <p class="text-danger">{{$errors->first('email')}}</p>
                                                 @endif
@@ -43,7 +43,7 @@
                                             <div class="form-group mb-15">
                                                 <label for="password-field">password</label>
                                                 <div class="position-relative">
-                                                    <input id="password-field" type="password" class="form-control" name="password" placeholder="Password" value="admin">
+                                                    <input id="password-field" type="password" class="form-control" name="password" placeholder="Password">
                                                     <span toggle="#password-field" class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2"></span>
                                                 </div>
                                                 @if($errors->has('password'))
